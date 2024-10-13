23dc90637754e0984c7d1f263ed4efb4edbb46e3
Manodeepray <127145195+Manodeepray@users.noreply.github.com>
2024-10-13 23:00:42 +0530

    updating the repo

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from tools import questions, RAG, image_pred
 state = {"grade": "none", "stage": "none"}
 
 def run(image):
-    exit_image, class_prediction, grade, stage, location= image_pred.image_prediction(image)
-    
-    grade = grade.replace('_', ' ')
-    stage = stage.replace('_', ' ')
-    location = location.replace('_', ' ')
+    exit_image, class_prediction, grade, stage , _= image_pred.image_prediction(image)
+    # Update the shared state
+    state["grade"] = grade
+    state["stage"] = stage
+    return exit_image, class_prediction, grade, stage
 
+    '''def answer(symptoms, Duration_and_Progressions, Bleeding_or_Swellings, Lifestyle_and_Habits, Medical_and_Family_History, Oral_Hygiene_and_Dental_History, Lesion_Descriptions, Lymph_Node_Involvements, Biopsy_and_Pathology_Reports, General_Health_and_Functional_Impacts, Overall_Health, Impacts_on_Daily_Life, Treatment_History_and_Responses, grade, stage):
+    prompt = questions.prompt_return(symptoms, Duration_and_Progressions, Bleeding_or_Swellings, Lifestyle_and_Habits, Medical_and_Family_History, Oral_Hygiene_and_Dental_History, Lesion_Descriptions, Lymph_Node_Involvements, Biopsy_and_Pathology_Reports, General_Health_and_Functional_Impacts, Overall_Health, Impacts_on_Daily_Life, Treatment_History_and_Responses)
+    query = f"what would be the grade  on the basis of following prompt : {prompt}"
+    
     
     query1 = f"""what would be the teatment when {grade} ." """  
     query2 = f"""what would be the teatment when {stage} . """
@@ -22,10 +26,78 @@ def run(image):
     print("\nfinal_answer2 : ",final_answer2) 
     #cancer, final_answer = RAG.RAG_output(query)
     final_answer = f"treatment for {grade}   :" + final_answer1 + f"\n Treatment for {stage} :"+final_answer2
-    cancer = "cancer is present on "+location+f" and of {grade} and {stage}"
+    cancer = Lesion_Descriptions+f" and of {grade} and {stage}"
+    return str(query), cancer, final_answer'''
+def answer(Duration , Progression , Bleeding , Swellings ,
+           
+                                                 Persistent_sore , Smoke , Alchohol , Oral_Hygiene ,
+                                                 Dentist , Oral_infection , Lesion_Size , Lesion_Size_change ,
+                                                 Lesion_Colour , Lesion_Colour_change , Lesion_Location ,
+                                                 Lymph_Node_Involvements , Overall_Health , Family_History ,
+                                                 Medical , Treatment):
     
-    return exit_image, class_prediction, grade, stage ,location,cancer, final_answer
+    
+    grade_query = f"""what would be the grade on the basis of following prompt :     
+            duration ={Duration} 
+            gotten worse={Progression} 
+            bleeding={Bleeding} 
+            swelling={Swellings} 
+            persistent soreSwellings = {Persistent_sore} 
+            smoke if yes how much ={Smoke} 
+            alcohol, if yes how much ={Alchohol} 
+            brush, floss ={Oral_Hygiene} 
+            dentist={Dentist} 
+            oral infection ={Oral_infection} 
+            lesion size = {Lesion_Size} 
+            change in lesion size ={Lesion_Size_change} 
+            lesion colour = {Lesion_Colour} 
+            change in lesion colour={Lesion_Colour_change} 
+            lesion-location={Lesion_Location} 
+            lymph node (swollen area in neck) ={Lymph_Node_Involvements} 
+            overall health={Overall_Health} 
+            family history of cancer={Family_History} 
+            serious illness before={Medical} 
+            treatment on cancer={Treatment}"""
+            
+            
+    stage_query = f"""what would be the stage on the basis of following prompt :     
+            duration ={Duration} 
+            gotten worse={Progression} 
+            bleeding={Bleeding} 
+            swelling={Swellings} 
+            persistent soreSwellings = {Persistent_sore} 
+            smoke if yes how much ={Smoke} 
+            alcohol, if yes how much ={Alchohol} 
+            brush, floss ={Oral_Hygiene} 
+            dentist={Dentist} 
+            oral infection ={Oral_infection} 
+            lesion size = {Lesion_Size} 
+            change in lesion size ={Lesion_Size_change} 
+            lesion colour = {Lesion_Colour} 
+            change in lesion colour={Lesion_Colour_change} 
+            lesion-location={Lesion_Location} 
+            lymph node (swollen area in neck) ={Lymph_Node_Involvements} 
+            overall health={Overall_Health} 
+            family history of cancer={Family_History} 
+            serious illness before={Medical} 
+            treatment on cancer={Treatment}"""
+    
+    grade , stage = RAG.RAG_cancer_output(grade_query , stage_query)
+    
+    CANCER = f"cancer output :The grade is: {grade} and the stage is: {stage}"
+    print(f"cancer output :The grade is: {grade} and the stage is: {stage}")
 
+    
+    
+    query1 = f"""what would be the teatment when grade is {grade} ." """  
+    query2 = f"""what would be the teatment when stage is {stage} . """
+    final_answer1 = RAG.RAG_output_treatment(query1)
+    print("\nfinal_answer1 : ",final_answer1)
+    final_answer2 = RAG.RAG_output_treatment(query2)
+    print("\nfinal_answer2 : ",final_answer2) 
+    
+    return  CANCER, final_answer1 , final_answer2
+    
     
 
 # Function to handle the "Continue" button click
@@ -36,26 +108,89 @@ with gr.Blocks() as app:
     with gr.Row():
         with gr.Column(visible=True) as prediction_app:
             gr.Markdown("# Disease Prediction App")
-            image_input = gr.Image(type="pil")
-            image_output = gr.Image()
+            image_input = gr.Image(type="pil", label="Input Image" , width=700, height=700)
+            image_output = gr.Image(label="observed Image", width=700, height=700)
             class_output = gr.Textbox(label="Observation")
             grade_output = gr.Textbox(label="Grade")
             stage_output = gr.Textbox(label="Stage")
-            location_output = gr.Textbox(label="Location")
-            cancer_output = gr.Textbox(label="Cancer")
-            treatment_output = gr.Textbox(label="Treatment")
             btn = gr.Button("Predict")
             continue_btn = gr.Button("Continue if cancer is detected", visible=False)
 
-            btn.click(run, inputs=image_input, outputs=[image_output, class_output, grade_output, stage_output,location_output,cancer_output,treatment_output])
+            btn.click(run, inputs=image_input, outputs=[image_output, class_output, grade_output, stage_output])
+            btn.click(fn=lambda: gr.update(visible=True), inputs=None, outputs=continue_btn)
+
+        with gr.Column(visible=False) as chatbot_app:
             
+            Duration = gr.Textbox(label=questions._duration)
+            Progression = gr.Textbox(label=questions._gotten_worse)
+            Bleeding = gr.Textbox(label=questions._bleeding)
+            Swellings = gr.Textbox(label=questions._swelling)
+            Persistent_sore = gr.Textbox(label=questions._persistent_sore)
+            Smoke = gr.Textbox(label=questions._smoke_)
+            Alchohol = gr.Textbox(label=questions._alcohol_)
+            Oral_Hygiene=  gr.Textbox(label=questions._brush_floss_)
+            Dentist = gr.Textbox(label= questions._dentist)
+            Oral_infection = gr.Textbox(label=questions._oral_infection_)
+            Lesion_Size = gr.Textbox(label=questions._lesion_size_)
+            Lesion_Size_change = gr.Textbox(label=questions._change_in_lesion_size_)
+            Lesion_Colour = gr.Textbox(label=questions._lesion_colour_)
+            Lesion_Colour_change = gr.Textbox(label=questions._change_in_lesion_colour)
+            Lesion_Location = gr.Textbox(label=questions._lesion_location)
+            Lymph_Node_Involvements = gr.Textbox(label=questions._lymph_node_)
+            Overall_Health = gr.Textbox(label=questions._overall_health)
+            Family_History = gr.Textbox(label=questions._family_history_of_cancer)
+            Medical  = gr.Textbox(label=questions._serious_illness_before)
+            Treatment =  gr.Textbox(label=questions._treatment_on_cancer)
+            
+            
+            
+            
+            
+            '''location = gr.Textbox(label="enter the location from the image model")
+            grade = gr.Textbox(label="enter the grade from the image model")
+            stage = gr.Textbox(label="enter the stage from the image model" )'''
+            
+            
+            
+            #output = gr.Textbox(label="Your Answer:")
+            patient_cancer = gr.Textbox(label="Patient has the following cancer:")
+            
+            treatment1 = gr.Textbox(label="Treatment generated from RAG:")
+            treatment2 = gr.Textbox(label="Treatment generated from RAG:")
+            
+            process_btn = gr.Button("Process")
+
+            #process_btn.click(fn=answer, inputs=[symptoms, Duration_and_Progressions, Bleeding_or_Swellings, Lifestyle_and_Habits, Medical_and_Family_History, Oral_Hygiene_and_Dental_History, Lesion_Descriptions, Lymph_Node_Involvements, Biopsy_and_Pathology_Reports, General_Health_and_Functional_Impacts, Overall_Health, Impacts_on_Daily_Life, Treatment_History_and_Responses,grade,stage], outputs=[output, patient_cancer, treatment])
+            process_btn.click(fn=answer, inputs=[Duration , Progression , Bleeding , Swellings ,
+                                                 Persistent_sore , Smoke , Alchohol , Oral_Hygiene ,
+                                                 Dentist , Oral_infection , Lesion_Size , Lesion_Size_change ,
+                                                 Lesion_Colour , Lesion_Colour_change , Lesion_Location ,
+                                                 Lymph_Node_Involvements , Overall_Health , Family_History ,
+                                                 Medical , Treatment], outputs=[patient_cancer, treatment1,treatment2])
+
+        
+                
+        continue_btn.click(fn=continue_to_chatbot, inputs=None, outputs=[prediction_app, chatbot_app])
+
     app.launch(server_name="127.0.0.1", server_port=7860)
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+##################################################################################################
 ''' 
 #if state["grade"] != 'none' or state["stage"] != 'none':
 
